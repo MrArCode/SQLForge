bbe62ca0886cd6da4ed175fd795af8f6e6fed270
Artur Cegiełka-Posłuszny <a.c.posluszny@gmail.com>
2024-11-03 22:25:41 +0100

    Updating README.MD

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("java")
-    id("maven-publish") // Dodanie wtyczki maven-publish
+    id("maven-publish")
 }
 
 group = "com.github.MrArCode"
@@ -13,23 +13,13 @@ repositories {
 
 dependencies {
     implementation("com.github.javafaker:javafaker:1.0.2")
+
     compileOnly("org.projectlombok:lombok:1.18.34")
     annotationProcessor("org.projectlombok:lombok:1.18.34")
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
-
-publishing {
-    publications {
-        create<MavenPublication>("mavenJava") {
-            from(components["java"])
-            groupId = "com.github.MrArCode"
-            artifactId = "SQLForge"
-            version = "0.1.0"
-        }
-    }
-}
-
 tasks.test {
     useJUnitPlatform()
 }
