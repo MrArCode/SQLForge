7b123bffadab248588f67d259a318296499a9436
Artur Cegiełka-Posłuszny <a.c.posluszny@gmail.com>
2024-11-03 21:54:06 +0100

    Fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,22 @@
 plugins {
     id("java")
+    id("maven-publish")
 }
 
-group = "org.example"
-version = "1.0-SNAPSHOT"
+group = "com.github.MrArCode"
+version = "0.1.0"
 
 repositories {
     mavenCentral()
+    maven { url = uri("https://jitpack.io") }
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    implementation ("com.github.javafaker:javafaker:1.0.2")
-
+    implementation("com.github.javafaker:javafaker:1.0.2")
     compileOnly("org.projectlombok:lombok:1.18.34")
     annotationProcessor("org.projectlombok:lombok:1.18.34")
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
 tasks.test {
