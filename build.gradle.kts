9b1229ba460ffb39485e0391a2eed0cd9f750c18
Artur Cegiełka-Posłuszny <a.c.posluszny@gmail.com>
2024-11-03 21:05:03 +0100

    Adding readme

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,15 +7,18 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven { url = uri("https://jitpack.io") } // Dodaje JitPack jako repozytorium
 }
 
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-    implementation ("com.github.javafaker:javafaker:1.0.2")
+    implementation("com.github.javafaker:javafaker:1.0.2")
 
     compileOnly("org.projectlombok:lombok:1.18.34")
     annotationProcessor("org.projectlombok:lombok:1.18.34")
+
+    implementation("com.github.MrArCode:SQLForge:1.0.0")
 }
 
 tasks.test {
